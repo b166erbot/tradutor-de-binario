1c778e65def69adce857419475a89fbbbedcd683
b166erobot <b166erobot@outlook.com>
2019-09-02 19:41:29 -0300

    .

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ setup(
     name='tradutor-de-binario',
     version='1.0.0',
     description='Um tradutor de binário para ascii (texto) fácil de usar',
-    url='https://github.com/bernardofreitas/tradutor-de-binario',
-    author='Bernardo Freitas',
-    author_email='bernard0freitas@outlook.com',
+    url='https://github.com/b166erobot/tradutor-de-binario',
+    author='b166erobot',
+    author_email='b166erobot@outlook.com',
     packages=find_packages(),
     scripts=['tradutor.py'],
     install_requirements=['click']
     )
-
